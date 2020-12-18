b83b3c5efb6d3927540bb175b4055ced5f836f66
fanny <fanny.miech@le-campus-numerique.fr>
2020-12-18 09:43:22 +0100

    first commit

---
+++ ennemy_ms/src/main/java/com/dd_dreamteam/ennemy_ms/EnnemyMsApplication.java
@@ -0,0 +1,14 @@
+package com.dd_dreamteam.ennemy_ms;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnnemyMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnnemyMsApplication.class, args);
+	}
+
+}
